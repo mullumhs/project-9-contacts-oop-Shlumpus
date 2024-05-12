4e7ba45afce219699602b66981f1eba2f78a882b
Shlumpus <140124184+Shlumpus@users.noreply.github.com>
2024-05-12 23:48:10 +0000

    contacts classes

--- lab2.py
+++ lab2.py
@@ -11,13 +11,23 @@
 # This class should have an initialiser with attributes for name, phone_number, and email.
 # Add a class attribute to keep track of the total number of contacts.
 
+class Contacts:
+    number_of_contacts = 0
+    
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        Contacts.number_of_contacts += 1
+        
+contact1 = Contacts("Bill Smith", "350285", "billsmith@cheese.com")
+contact2 = Contacts("Grug Williams", "135187", "grug123@a.site.com")
 
-
-# Create at least two instances of the Contact class with different data.
+        # Create at least two instances of the Contact class with different data.
 
 
 
 # Write code that prints out the details of each contact you have created.
 
-
+print(contact1.name)
 
